0d96c2e0a76346c7a7fecd57fc8b1c70a55fbe2c
mrjordantanner <mrjordantanner@gmail.com>
2021-04-23 10:56:13 -0500

    Edit CORS

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const APIurl = 'https://jts-clique-backend.herokuapp.com/';
-// const APIurl = 'http://localhost:8080';  
+// const APIurl = 'https://jts-clique-backend.herokuapp.com/';
+const APIurl = 'http://localhost:8080';  
 
 export default APIurl;
